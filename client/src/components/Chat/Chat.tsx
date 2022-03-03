4c3ab4d81a361dc5c4c7b7e7fff354cdc345c40c
arezzinskikh <razzi_alex@mac.org.il>
2022-03-03 17:35:01 +0200

    refactoring

--- client/src/components/Chat/Chat.tsx
+++ client/src/components/Chat/Chat.tsx
@@ -1,58 +1,61 @@
-import React, { FC, useEffect, useState } from 'react';
-import styles from './Chat.module.css';
-import socket from '../../socket/socket';
-import {IUser} from '../../models/User';
-import {IMessage} from '../../models/Message';
-import ReactScrollToBottom from '@benarnold/react-scroll-to-bottom';
+import React, { FC, useEffect, useState } from "react";
+import styles from "./Chat.module.css";
+import socket from "../../socket/socket";
+import { IUser } from "../../models/User";
+import { IMessage } from "../../models/Message";
 
 interface ChatProps {
-    user:IUser,
-    roomId: string,
-    messageList:IMessage[],
-    setMessageList:Function
+  user: IUser;
+  roomId: string;
+  messageList: IMessage[];
+  setMessageList: Function;
 }
-export const Chat:FC<ChatProps> = ({ user, roomId, messageList, setMessageList}) => {
-    const [value, setValue] = useState('');
- 
-    useEffect(():any => {
-        socket.on('receive_message', (message) => {
-            setMessageList((list:any) => [...list, message]);
-        });
-        return () => socket.disconnect();
-    }, [socket]);
+export const Chat: FC<ChatProps> = ({
+  user,
+  roomId,
+  messageList,
+  setMessageList,
+}) => {
+  const [value, setValue] = useState<any>("");
 
-    const sendMessage = async (e:React.ChangeEvent<HTMLButtonElement>) => {
-        console.log(user.username);
-        e.preventDefault();
-        const NewMessage:IMessage = {
-            //???? it's ok?
-            username: user.username || '',
-            message: value,
-            roomId
-        };
-        await socket.emit('newMessage', NewMessage);
-        setValue('');
+  useEffect((): any => {
+    socket.on("receive_message", (message) => {
+      setMessageList((list: any) => [...list, message]);
+    });
+    return () => socket.disconnect();
+  }, [setMessageList]);
+
+  const handleMessageButton = async (e: any) => {
+    e.preventDefault();
+    const newMessage: IMessage = {
+      //???? it's ok?
+      username: user.username || "",
+      message: value,
+      roomId,
     };
+    await socket.emit("newMessage", newMessage);
+    setValue("");
+  };
 
-    return (
-        <div className='chat'>
-            <div className='createMessage'>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => setValue(e.target.value)}
-                />
-                <button className={styles.msgButton} onClick={() => sendMessage}>
-                    Send
-                </button>
-            </div>
-            <ReactScrollToBottom className={styles.messages}>
-                {messageList.map((message:IMessage) => (
-                    <div className={styles.oneMessage} key={message._id}>
-                        {message.username}: {message.message} 
-                    </div>
-                ))}
-            </ReactScrollToBottom>
-        </div>
-    );
+  const handleMessageChange = async (e: any) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div className={styles.chat}>
+      <div className={styles.messages}>
+        {messageList.map((message: IMessage) => (
+          <div className={styles.oneMessage} key={message._id}>
+            {message.username}: {message.message}
+          </div>
+        ))}
+      </div>
+      <div className={styles.createMessage}>
+        <input type="text" value={value} onChange={handleMessageChange} />
+        <button className={styles.msgButton} onClick={handleMessageButton}>
+          Send
+        </button>
+      </div>
+    </div>
+  );
 };
--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-    db: 'mongodb+srv://alex:alex123@cluster0.theuu.mongodb.net/ChatApp?retryWrites=true&w=majority',
-    jwt: 'dev-jwt'
-}
+  db: "mongodb+srv://alex:alex123@cluster0.theuu.mongodb.net/ChatApp?retryWrites=true&w=majority",
+  jwt: "dev-jwt",
+};
